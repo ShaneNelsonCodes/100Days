237a7bc4c65991b230a704b430259d2508da1a02
snelson1838 <39141549+snelson1838@users.noreply.github.com>
2020-12-05 19:28:59 -0500

    commit

---
+++ Day6 - Escaping the Maze.py
@@ -0,0 +1,21 @@
+website = r"https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json"
+
+# solution
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+    
+while at_goal() == False:
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
