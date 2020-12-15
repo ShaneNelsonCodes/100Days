268fc89e75137c9d2c18522ca789bc832e6d6cb8
snelson1838 <39141549+snelson1838@users.noreply.github.com>
2020-12-15 08:33:02 -0500

    Initial commit

---
+++ Day13_Debugging.py
@@ -0,0 +1,11 @@
+# Starting code had issues and then were fixed
+
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
